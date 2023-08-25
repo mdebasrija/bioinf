5466c7c4388b0bececbbf530ec0f366dbc6e7568
Debasrija Mondal <mdebasrija@gmail.com>
2023-08-25 10:50:42 +0530

    added answers to some questions in comments

--- mtNd1 - Assignment 03.R
+++ mtNd1 - Assignment 03.R
@@ -1,7 +1,8 @@
 setwd("~/Desktop/bioinf")
 mtNd1 = readLines("mtNd1.fasta") #load and store fasta file
+#There are 5 sequences.
 table(strsplit(mtNd1[2], "")) #split line 2 of this file, with spaces
-#this gives the number of A, C, T, G in that line
+#this gives the number of A, C, T, G in that line - there are 13 As
 
 #this for loop prints the first character in every line
 for(line in mtNd1){
