70b9eb7a73769af21a393d3cc73ce212a94701ec
Debasrija Mondal <mdebasrija@gmail.com>
2023-08-25 09:57:43 +0530

    Assignment 03 - mtNd1

---
+++ mtNd1 - Assignment 03.R
@@ -0,0 +1,19 @@
+setwd("~/Desktop/bioinf")
+mtNd1 = readLines("mtNd1.fasta") #load and store fasta file
+table(strsplit(mtNd1[2], "")) #split line 2 of this file, with spaces
+#this gives the number of A, C, T, G in that line
+
+#this for loop prints the first character in every line
+for(line in mtNd1){
+  firstletter = unlist(strsplit(line, ""))[1]
+  cat(firstletter, "\n")
+}
+
+#this loop prints the category of every line, based on the first character
+#> for identifier, NA for empty and the others for DNA sequence (thus assuming no errors)
+for(line in mtNd1){
+  firstletter = unlist(strsplit(line, ""))[1]
+  if(is.na(firstletter)) {cat("EMPTY\n")}
+  else if(firstletter=='>') {cat("IDENTIFIER\n")}
+  else {cat("DNA Sequence\n")}
+}
